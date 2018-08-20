7ddef0af8c2380fdf99d40c51a49d6720f323672
Ismael Medeiros <140083162@aluno.unb.br>
2018-08-20 13:41:54 -0300

    .NET MVC: Updated homepage bundle; Added controllers for list certificates sample.

---
+++ dotnet/mvc/PkiSuiteAspNetMvcSample/Controllers/ListCertJQueryController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PkiSuiteAspNetMvcSample.Controllers {
+	public class ListCertJQueryController : Controller {
+
+		// GET: ListCertJQuery
+		public ActionResult Index() {
+			return View();
+		}
+	}
+}
