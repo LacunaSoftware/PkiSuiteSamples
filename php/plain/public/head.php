4a42d62648b731f7d279533e556eabb56e028eb6
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2018-12-20 16:50:37 -0200

    PHP/Plain: Changed bundle to get from real CDN.

--- php/plain/public/head.php
+++ php/plain/public/head.php
@@ -6,4 +6,4 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" integrity="sha384-RdQbeSCGSeSdSlTMGnUr2oDJZzOuGjJAkQy1MbKMu8fZT5G0qlBajY0n0sY/hKMK" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdn.lacunasoftware.com/lacuna.31ae2c49ab114167d42a.css" />
+<link rel="stylesheet" href="https://cdn.lacunasoftware.com/pki-suite-samples/lacuna.31ae2c49ab114167d42a.css" />
