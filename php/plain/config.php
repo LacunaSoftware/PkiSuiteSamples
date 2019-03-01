51e44af20fcda8c7ecab6f16002aec018032dc73
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2019-03-01 19:25:35 -0300

    Updated tokens and licenses.

--- php/plain/config.php
+++ php/plain/config.php
@@ -20,8 +20,8 @@ function getConfig()
             // ====================================================
             //     >>>> PASTE YOUR REST PKI ACCESS TOKEN BELOW <<<<
             // ====================================================
-            'accessToken' => 'QPK_s7eVFFitenHh-6c5PBEk1nlssA5OQkbpyZWBnGEvG7cK3lTs4Toxs2STulwrRPORtJbT8m9V_hR_FTZTPUnzqgcWC4xmd7QYPJZIXBIw2HgsjTc6GAEL6N1TXSBLBNDz_FkwwnlKCk53YPBa4AAnWZBMfe5riah1F_BpinrxKGX1ewohIgxTHxzAeRtQcwRB8XW6yk9TR6GL7mK5ky0o2u00VnSgzrOLd8F0cuV44257dZlumMNTBkq2fBeAUWTGNo3Ydwgn5k5BYM2hfCdWDssiRv-GLyqxjo7t4brq2hUK6QBXisMXfZ4dSd_rRQvpVpa1cdS4KansOxanQX9M2fNU8Gmf6I22SwZ0c5wM9qlkv5Q-LZOYtakrkdqxY-lCUDog9goPCBQenSt128thFko3uFKdAyiZCiL5fTi9I-ZBGNbNB7rf6nACvlWAH_jkGpfSFnrarSbtzniVXI7wSf8ge4iCeP92HT9CNSbRkGnBYRl-ryr39nqz7--vYpJ6kw',
-            // This is a TRIAL token. It will be expired at 01/04/2019.
+            'accessToken' => 'zF1ozzyD_Vw67x3kKrdgbN9OjqgEvDrApbJogKtjfNxqvnQpS6jRfUPONGFhIZMDnLrR_Hp_vRWIk6LrzwrxLkOmGOFUyhTh2SC0FFCQuCE-Jv2KjL556IMOPxkQwncCxcFkQHdxVDpslFIezY29CGp0rWLCLXuPJp9EVkUlR48BofZE7J0XFvMx-TkZtg1bCsQWKdLtyMgQJxsLz1OQJ-XbPnWN-lEpp2tfJmSRQXLvIYnSAe5oSr-s89C6T_CmIYpZBRJsAjHYgky7Vk5lSDPZHs99F7NAoBZ8TdDUs7DenfTJ23ONmSqyHHPEKoXgqAb5-rQZ3S0YDDzAqPFwAJ84bXaHNMueDjvIWwgcIjfHvzEVf0RpvRvNirnUsDaMsy2ipgO_uT1npKnAuVAPV0tJM6Ci8jTmIVk4BBb_Ju6dxKJdpEyyCzDNFfokks2txcMSkpOAxQmlTJsqyIY8K8JRWULjujW4rjGNpFcr_QuY_919WKm-ouTzLoS8eUctnFIUcg',
+            // This is a TRIAL token. It will be expired at 30/04/2019.
             // If the REST PKI sample doesn't work, please contact our support by email: suporte@lacunasoftware.com
 
             // Address of your Rest PKI installation (with the trailing '/' character)
