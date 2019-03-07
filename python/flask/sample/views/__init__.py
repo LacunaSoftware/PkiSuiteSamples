f2d1701bc5440413363c0ce138b9cdf551e8ba30
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2019-03-07 14:25:15 -0300

    Python/Flask: Added signature with server key samples.

--- python/flask/sample/views/__init__.py
+++ python/flask/sample/views/__init__.py
@@ -6,12 +6,14 @@ from .batch_pades_signature_express import blueprint as \
     batch_pades_signature_express
 from .batch_pades_signature_restpki import blueprint as \
     batch_pades_signature_restpki
+from .cades_server_key_express import blueprint as cades_server_key_express
 from .cades_signature_express import blueprint as cades_signature_express
 from .cades_signature_restpki import blueprint as cades_signature_restpki
 from .check_pades_express import blueprint as check_pades_express
 from .download import blueprint as download
 from .home import blueprint as home
 from .open_pades_express import blueprint as open_pades_express
+from .pades_server_key_express import blueprint as pades_server_key_express
 from .pades_signature_express import blueprint as pades_signature_express
 from .pades_signature_restpki import blueprint as pades_signature_restpki
 from .printer_version_pades_express import blueprint as \
@@ -25,12 +27,14 @@ blueprints = {
     batch_cades_rest,
     batch_pades_signature_express,
     batch_pades_signature_restpki,
+    cades_server_key_express,
     cades_signature_express,
     cades_signature_restpki,
     check_pades_express,
     download,
     home,
     open_pades_express,
+    pades_server_key_express,
     pades_signature_express,
     pades_signature_restpki,
     printer_version_pades_express,
