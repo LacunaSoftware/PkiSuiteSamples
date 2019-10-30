177f45fd8bd7c553fd079085e1bfd6047eb88dc2
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2019-10-30 17:28:21 -0300

    [.NET/MVC] Fixed dependency problem.

--- dotnet/mvc/PkiSuiteAspNetMvcSample/Global.asax.cs
+++ dotnet/mvc/PkiSuiteAspNetMvcSample/Global.asax.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -11,7 +10,6 @@ namespace PkiSuiteAspNetMvcSample {
 	public class MvcApplication : System.Web.HttpApplication {
 		protected void Application_Start() {
 			AreaRegistration.RegisterAllAreas();
-			GlobalConfiguration.Configure(WebApiConfig.Register);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 		}
 	}
