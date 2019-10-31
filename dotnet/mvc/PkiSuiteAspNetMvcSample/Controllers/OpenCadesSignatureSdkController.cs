8244984afb33da9811b2fd3310a4d587594f743d
Emily Souza <emily.souza.rs@gmail.com>
2019-10-31 17:29:11 -0300

    [.NET/MVC] Added Download Button at Cades Signature Validation Sample

--- dotnet/mvc/PkiSuiteAspNetMvcSample/Controllers/OpenCadesSignatureSdkController.cs
+++ dotnet/mvc/PkiSuiteAspNetMvcSample/Controllers/OpenCadesSignatureSdkController.cs
@@ -8,33 +8,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace PkiSuiteAspNetMvcSample.Controllers
-{
-    public class OpenCadesSignatureSdkController : BaseController
-    {
-        /**
+namespace PkiSuiteAspNetMvcSample.Controllers {
+	public class OpenCadesSignatureSdkController : BaseController {
+		/**
 		 * This action open a Cades file and inspect its signatures.
 		 */
-        public ActionResult Index(string userfile)
-        {
-            // Our action only works if a userfile is given to work with.
-            byte[] userfileContent;
-            if (!StorageMock.TryGetFile(userfile, out userfileContent))
-            {
-                return HttpNotFound();
-            }
-            // Open Cades file
-            var cadesSignature = CadesSignature.Open(userfileContent);
+		public ActionResult Index(string userfile) {
+			// Our action only works if a userfile is given to work with.
+			byte[] userfileContent;
+			if (!StorageMock.TryGetFile(userfile, out userfileContent)) {
+				return HttpNotFound();
+			}
+			// Open Cades file
+			var cadesSignature = CadesSignature.Open(userfileContent);
 
-            // Specify the parameters for the signature validation:
-            // Define the trust arbitrator used to validate the certificate.
-            var trustArbitrator = Util.GetTrustArbitrator();
-            var policyMapper = CadesPoliciesForValidation.GetCadesBasic(trustArbitrator);
+			cadesSignature.SetExternalData(StorageMock.GetSampleDocContent());
 
-            return View(new OpenCadesSignatureModel()
-            {
-                Signature = new CadesSignatureModel(cadesSignature, policyMapper)
-            });
-        }
-    }
+			// Specify the parameters for the signature validation:
+			// Define the trust arbitrator used to validate the certificate.
+			var trustArbitrator = Util.GetTrustArbitrator();
+			var policyMapper = CadesPoliciesForValidation.GetCadesBasic(trustArbitrator);
+
+			return View(new OpenCadesSignatureModel() {
+				File = cadesSignature.HasEncapsulatedContent ? StorageMock.Store(cadesSignature.GetEncapsulatedContent()) : "",
+				Signature = new CadesSignatureModel(cadesSignature, policyMapper)
+			});
+		}
+	}
 }
