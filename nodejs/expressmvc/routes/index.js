469cf49677fa3e8635f622996a51976570f10a55
Emily Souza <emily.souza.rs@gmail.com>
2019-11-12 12:58:23 -0300

    [Node.js/Express MVC] Added PDF signature validation sample using REST PKI

--- nodejs/expressmvc/routes/index.js
+++ nodejs/expressmvc/routes/index.js
@@ -11,6 +11,7 @@ router.use('/batch-pades-signature-express', require('./batch-pades-signature-ex
 router.use('/cades-signature-restpki', require('./cades-signature-restpki'));
 router.use('/download', require('./download'));
 router.use('/open-pades-express', require('./open-pades-express'));
+router.use('/open-pades-restpki', require('./open-pades-restpki'));
 router.use('/pades-signature-express', require('./pades-signature-express'));
 router.use('/pades-signature-restpki', require('./pades-signature-restpki'));
 router.use('/upload', require('./upload'));
--- nodejs/expressmvc/routes/open-pades-restpki.js
+++ nodejs/expressmvc/routes/open-pades-restpki.js
@@ -2,14 +2,14 @@ const express = require('express');
 const {
     PadesSignatureExplorer,
     StandardSignaturePolicies
-} = require('pki-express');
+} = require('restpki-client');
 const { StorageMock } = require('../storage-mock');
 const { Util } = require('../util');
 
 const router = express.Router();
 
 /**
- * GET /open-pades-express
+ * GET /open-pades-restpki
  *
  * This route only submits a PDF file to PKI Express for inspection of its signatures.
  */
@@ -29,27 +29,27 @@ router.get('/', (req, res, next) => {
 
     // Get an instance of PadesSignatureExplorer class, used to open/validate PDF
     // signatures.
-    sigExplorer = new PadesSignatureExplorer();
-    Util.setPkiDefaults(sigExplorer);
+    sigExplorer = new PadesSignatureExplorer(Util.getRestPkiClient());
 
     // Specify that we want to validate the signatures in the file, not only
     // inspect them.
     sigExplorer.validate = true;
 
-    // Set the PDF file.
-    sigExplorer.setSignatureFileFromPathSync(filePath);
-
     // Specify the parameters for the signature validation:
     // Accept any PAdES signature as long as the signer has an ICP-Brasil
     // certificate.
-    sigExplorer.signaturePolicy = StandardSignaturePolicies.PADES_BASIC;
+    sigExplorer.defaultSignaturePolicyId = StandardSignaturePolicies.PADES_BASIC;
 
     // Specify the security context to be used to determine trust in the
     // certificate chain. We have encapsulated the security context on util.js.
     sigExplorer.securityContextId = Util.getSecurityContextId(res.locals.environment);
-
-    // Call the open() method, which returns the signature file's information.
-    sigExplorer.open().then(r=> res.render('open-pades-express', { signature: r }));
+    
+    // Set the PDF file.
+    sigExplorer.setSignatureFileFromPath(filePath);
+   
+    // Call the open() method, which returns a Promise with the signature file's 
+    // information.
+    sigExplorer.open().then(r=> res.render('open-pades-restpki', { signature: r }));
 });
 
 module.exports = router;
