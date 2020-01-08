c8353c5707221f86585d2b728ea9f8b2aff933ff
Emily <emily.souza.rs@gmail.com>
2020-01-08 15:31:10 -0300

    [Node.js/Express MVC] Added batch CAdES signature using REST PKI and fixed batch PAdES signature using REST PKI

---
+++ nodejs/expressmvc/routes/batch-cades-signature-restpki.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const path = require('path');
+const uuidv4 = require('uuid/v4');
+const { CadesSignatureStarter, StandardSignaturePolicies, CadesSignatureFinisher } = require('restpki-client');
+
+const { Util } = require('../util');
+const { StorageMock } = require('../storage-mock');
+
+let router = express.Router();
+let appRoot = process.cwd();
+
+router.get('/', (req, res, next) => {
+	// It is up to your application's business logic to determine which documents
+	// will compose the batch.
+	let documentsIds = Util.range(1, 30);
+
+	// Render the batch signature signature page.
+	res.render('batch-cades-signature-restpki', {
+		documentsIds: documentsIds
+	});
+});
+
+router.post('/start', (req, res, next) => {
+	// Get the parameters for this signature (received from the POST call via
+	// AJAX, see batch-signature-form.js).
+	let id = req.body['id'];
+	
+	// Get an instantiate of the CadesSignatureStarter class, responsible for
+	// receiving the signature elements and start the signature process.
+	signatureStarter = new CadesSignatureStarter(Util.getRestPkiClient());
+
+	// Set the document to be signed based on its ID.
+	signatureStarter.setFileToSignFromPath(StorageMock.getBatchDocPath(id));
+
+	// Set the signature policy.
+	signatureStarter.signaturePolicy = StandardSignaturePolicies.PKI_BRAZIL_CADES_ADR_BASICA;
+
+	// Set a security context. We have encapsulated the security context
+	// choice on util.py.
+	signatureStarter.securityContext = Util.getSecurityContextId(res.locals.environment);
+
+	// Optionally, set whether the content should be encapsulated in the
+	// resulting CMS. If this parameter is ommitted, the following rules
+	// apply:
+	// - If no CmsToCoSign is given, the resulting CMS will include the
+	// content.
+	// - If a CmsToCoSign is given, the resulting CMS will include the
+	// content if and only if the CmsToCoSign also includes the content.
+	signatureStarter.encapsulateContent = true;
+
+	// Call the start_with_webpki() method, which initiates the signature.
+	// This yields the token, a 43-character case-sensitive URL-safe string,
+	// which identifies this signature process. We'll use this value to call
+	// the signWithRestPki() method on the Web PKI component (see
+	// signature-form.js) and also to complete the signature after
+	// the form is submitted (see method action()). This should not be
+	// mistaken with the API access token.
+	return signatureStarter.startWithWebPki().then((result) => {
+	   // Return a JSON with the token obtained from REST PKI (the page will use
+	   // jQuery to decode this value).
+	   return res.json(result.token);
+	}).catch((err) => next(err));
+});
+
+router.post('/complete', (req, res, next) => {
+
+	// Get an instance of the CadesSignatureFinisher class, responsible for
+	// completing the signature process.
+	let signatureFinisher = new CadesSignatureFinisher(Util.getRestPkiClient());
+
+	// Set the token.
+	signatureFinisher.token = req.body.token;
+
+	// Call the finish() method, which finalizes the signature process.The
+	// return value is the CMS content.
+	return signatureFinisher.finish()
+		.then((result) => {
+
+			// The "certificate" property of the SignatureResult object contains
+			// information about the certificate used by the user to sign the file.
+			let signerCert = result.certificate;
+
+			// At this point, you'd typically store the signed PDF on your database.
+			// For demonstration purposes, we'll store the CMS on a temporary folder
+			// publicly accessible and render a link to it.
+			StorageMock.createAppData(); // Make sure the "app-data" folder exists (util.js).
+			let filename = uuidv4() + '.p7s';
+
+			// The SignatureResult object has functions for writing the signature file
+			// to a local life (writeToFile()) and to get its raw contents
+			// (getContent()). For large files, use writeToFile() in order to avoid
+			// memory allocation issues.
+			result.writeToFileSync(path.join(appRoot, 'app-data', filename));
+
+			// Render the result page.
+			return res.json(filename);
+		})
+		.catch((err) => next(err));
+});
+
+module.exports = router;
--- nodejs/expressmvc/routes/batch-pades-signature-restpki.js
+++ nodejs/expressmvc/routes/batch-pades-signature-restpki.js
@@ -37,7 +37,7 @@ router.get('/', (req, res, next) => {
  * This route is called asynchronously via AJAX by the batch signature page for
  * each document being signed. It receives the ID of the document and the
  * signer's certificate content and initiates a PAdES signature using
- * PKI Express and returns a JSON with the parameters for the client-side
+ * REST PKI and returns a JSON with the parameters for the client-side
  * signature using Web PKI (see batch-signature-form.js).
  */
 router.post('/start', (req, res, next) => {
@@ -45,45 +45,43 @@ router.post('/start', (req, res, next) => {
 	// Get the parameters for this signature (received from the POST call via
 	// AJAX, see batch-signature-form.js).
 	let id = req.body['id'];
-	let certContent = req.body['certContent'];
 
 	// Get an instantiate of the PadesSignatureStarter class, responsible for
 	// receiving the signature elements and start the signature process.
 	let signatureStarter = new PadesSignatureStarter(Util.getRestPkiClient());
 
 	// Set signature policy.
-	signatureStarter.signaturePolicy = StandardSignaturePolicies.PADES_BASIC_WITH_LTV;
+	signatureStarter.signaturePolicy = StandardSignaturePolicies.PADES_BASIC;
 
 	// Set PDF to be signed.
-	signatureStarter.setPdfToSignFromPathSync(StorageMock.getBatchDocPath(id));
+	signatureStarter.setPdfToSignFromPath(StorageMock.getBatchDocPath(id));
 
 	// Set Base64-encoded certificate's content to signature starter.
-	signatureStarter.setCertificateFromBase64Sync(certContent);
+	signatureStarter.securityContext = Util.getSecurityContextId();
+	
+	// Set the visual representation to the signature. We have encapsulated this
+	// code (on pades-visual-elements.js) to be used on various PAdES examples.
+	PadesVisualElementsRestPki.getVisualRepresentation()
+		.then(visualRepresentation => {
 
-	// Set a file reference for the stamp file. Note that this file can be
-	// referenced later by "fref://{alias}" at the "url" field on the visual
-	// representation (see public/vr.json or getVisualRepresentation()
-	// method).
-	signatureStarter.addFileReferenceSync('stamp', StorageMock.getPdfStampPath());
+			// Set the visual representation to signatureStarter.
+			signatureStarter.visualRepresentation = visualRepresentation;
 
-	// Set the visual representation. We provided a dictionary that represents
-	// the visual representation JSON model.
-	signatureStarter.setVisualRepresentationSync(PadesVisualElementsRestPki.getVisualRepresentation());
-
-	// Start the signature process. Receive as response the following fields:
-	// - toSignHash: The hash to be signed.
-	// - digestAlgorithm: The digest algorithm that will inform the Web PKI
-	//                    component to compute this signature.
-	// - transferFile: A temporary file to be passed to "complete" step.
-	signatureStarter.start()
-		.then(response => {
-
+			// Call the startWithWebPki() method, which initiates the signature.
+			// This yields the token, a 43-character case-sensitive URL-safe
+			// string, which identifies this signature process. We'll use this
+			// value to call the signWithRestPki() method on the WebPKI component
+			// (see public/js/signature-form.js) and also to complete the signature
+			// after the form is submitted (see post method). This should not be
+			// mistaken with the API access token.
+			return signatureStarter.startWithWebPki();
+		})
+		.then(result => {
 			// Respond this request with the fields received from start() method to
 			// be used on the javascript or on the complete action.
-			res.json(response);
+			res.json(result.token);
 		})
-		.catch(err => next(err));
-
+		.catch((err) => next(err));
 });
 
 /**
@@ -91,47 +89,40 @@ router.post('/start', (req, res, next) => {
  *
  * This route is called asynchronously via AJAX by the batch signature page for
  * each document being signed. It receives the ID of the document, the computed
- * signature and the transfer file, received on start action. We'll call PKI
- * Express to complete this signature and return a JSON with the saved filename
- * so that the page can render a link to it.
+ * signature and the transfer file, received on start action. We'll call REST
+ * PKI to complete this signature and return a JSON with the saved filename so
+ * that the page can render a link to it.
  */
 router.post('/complete', (req, res, next) => {
-
-	let outputFile = null;
-
-	// Get the parameters for this signature (received from the POST call via
-	// AJAX, see batch-signature-form.js).
-	let id = req.body['id'];
-	let transferFile = req.body['transferFile'];
-	let signature = req.body['signature'];
-
 	// Get an instance of the PadesSignatureFinisher class, responsible for
 	// completing the signature process.
 	let signatureFinisher = new PadesSignatureFinisher(Util.getRestPkiClient());
 
-	// Set PDF to be signed. It's the same file we used on "start" method.
-	signatureFinisher.setFileToSignFromPathSync(StorageMock.getBatchDocPath(id));
+	// Set the token.
+	signatureFinisher.token = req.body.token;
 
-	// Set transfer file.
-	signatureFinisher.setTransferFileFromPathSync(transferFile);
+	// Call the finish() method, which finalizes the signature process and
+	// returns the SignatureResult object.
+	signatureFinisher.finish()
+		.then((result) => {
+			// At this point, you'd typically store the signed PDF on you database.
+			// For demonstration purposes, we'll store the PDF on a temporary
+			// folder publicly accessible and render a link to it.
 
-	// Set signature.
-	signatureFinisher.signature = signature;
+			StorageMock.createAppData(); // Make sure the "app-data" folder exists.
+			let filename = uuidv4() + '.pdf';
 
-	// Generate path for output file and add the signature finisher.
-	StorageMock.createAppData(); // Make sure the "app-data" folder exists (util.js).
-	outputFile = uuidv4() + '.pdf';
-	signatureFinisher.outputFile = path.join(appRoot, 'app-data', outputFile);
+			// The SignatureResult object has functions for writing the signature
+			// file to a local life (writeToFile()) and to get its raw contents
+			// (getContent()). For large files, use writeToFile() in order to avoid
+			// memory allocation issues.
+			result.writeToFileSync(path.join(appRoot, 'app-data', filename));
 
-	// Complete the signature process.
-	signatureFinisher.complete()
-		.then(() => {
-			// Render a JSON with the saved filename (the page wil use jQuery to
-			// decode this value).
-			res.json(outputFile);
+			// Render the result page, showing the signed file and the signer
+			// certification info.
+			res.json(filename);
 		})
-		.catch(err => next(err));
-
+		.catch((err) => next(err));
 });
 
 module.exports = router;
