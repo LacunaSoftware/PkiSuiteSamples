b23d3dc66b85d5e7917e84f48fb700cd9db05ead
EmilyS <emily.souza.rs@gmail.com>
2020-03-18 18:46:38 -0300

    [Ruby/Rails] Added list and read certificates with jquery and select2

--- ruby/rails/config/routes.rb
+++ ruby/rails/config/routes.rb
@@ -14,6 +14,12 @@ Rails.application.routes.draw do
 
   get '/server-files/:rc/:op', action: :index, controller: :server_files, as: :server_files
   post '/server-files/:rc/:op', action: :action, controller: :server_files
+  
+  get '/list-cert-jquery/', action: :index, controller: :list_cert_jquery
+  get '/list-cert-select2/', action: :index, controller: :list_cert_select2
+  
+  get '/read-cert-jquery/', action: :index, controller: :read_cert_jquery
+  get '/read-cert-select2/', action: :index, controller: :read_cert_select2
 
   get '/pades-signature-rest/:file_id', action: :index, controller: :pades_signature_rest, as: :pades_signature_rest
   post '/pades-signature-rest/:file_id', action: :action, controller: :pades_signature_rest
