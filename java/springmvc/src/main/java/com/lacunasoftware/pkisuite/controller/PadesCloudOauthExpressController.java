760d255037fcd24022893c95defc501dc0b9cadb
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2020-08-20 15:49:06 -0300

    Update restpki-client package; Improved sample comments.

--- java/springmvc/src/main/java/com/lacunasoftware/pkisuite/controller/PadesCloudOauthExpressController.java
+++ java/springmvc/src/main/java/com/lacunasoftware/pkisuite/controller/PadesCloudOauthExpressController.java
@@ -17,12 +17,42 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
+
+/**
+ * This sample is responsible to perform a OAuth flow to communicate with PSCs to perform a
+ * signature. To perform this sample it's necessary to configure PKI Express with the credentials of
+ * the services by executing the following sample:
+ *
+ *    pkie config --set trustServices:<provider>:<configuration>
+ *
+ * All standard providers:
+ *    - BirdId
+ *    - ViDaaS
+ *    - NeoId
+ *    - RemoteId
+ *    - SafeId
+ * It's possible to create a custom provider if necessary.
+ *
+ * All configuration available:
+ *    - clientId
+ *    - clientSecret
+ *    - endpoint
+ *    - provider
+ *    - badgeUrl
+ *    - protocolVariant (error handling, normally it depends on the used provider)
+ *
+ * This sample will only show the PSCs that are configured.
+ */
 @Controller
 public class PadesCloudOauthExpressController {
 
 	// Redirect URL where it's accessed after OAuth flow is finished.
 	private final String REDIRECT_URL = "http://localhost:60695/pades-cloud-oauth-express/complete";
 
+	/**
+	 * This action will render a page that request a CPF to the user. This CPF is used to discover
+	 * which PSCs have a certificate containing that CPF.
+	 */
 	@GetMapping("/pades-cloud-oauth-express")
 	public ModelAndView get(
 		@RequestParam(value="fileId") String fileToSign
@@ -30,16 +60,32 @@ public class PadesCloudOauthExpressController {
 		return new ModelAndView("/pades-cloud-oauth-express/index");
 	}
 
+	/**
+	 * This action will be called after the user press the button "Search" on index page. It will
+	 * search for all PSCs that have a certificate with the provided CPF. Thus, it will start the
+	 * authentication process and return a URL to redirect the user to perform the authentication.
+	 *
+	 * After this action the user will be redirected, and to store the local data (fileId) to be user
+	 * after the user returns to your application. We use the parameter "customState", the last
+	 * parameter of the method discoverByCpfAndStartAuth(). This parameter will be recovered in the
+	 * next action.
+	 */
 	@PostMapping("/pades-cloud-oauth-express/discover")
 	public ModelAndView discover(
 		@RequestParam(value="fileId") String fileToSign,
 		@RequestParam(value="cpf") String cpf
 	) throws IOException {
-		TrustServicesManager manager = new TrustServicesManager();
 
-		// Process cpf
+		// Process cpf, removing all formatting.
 		String plainCpf = cpf.replaceAll("[.-]", "");
 
+		// Get an instance of the TrustServiceManager class, responsible for communicating with PSCs
+		// and handling the OAuth flow.
+		TrustServicesManager manager = new TrustServicesManager();
+
+		// Discover PSCs and receive a URL to redirect the user to perform the OAuth authentication
+		// page. As mentioned before, we pass the id of the file to be signed as the last parameter
+		// of the following method. The next action will recover this information.
 		List<TrustServiceAuthParameters> services = manager.discoverByCpfAndStartAuth(plainCpf, REDIRECT_URL, TrustServiceSessionTypes.SIGNATURE_SESSION, fileToSign);
 
 		// Render complete page.
@@ -49,13 +95,23 @@ public class PadesCloudOauthExpressController {
 		return new ModelAndView("/pades-cloud-oauth-express/discover", "model", model);
 	}
 
+	/**
+	 * This action will complete the authentication process and create a signature using a session
+	 * token returned by user. Also, we recover the parameter "customState" containing the id of the
+	 * file that will be signed.
+	 */
 	@GetMapping("/pades-cloud-oauth-express/complete")
 	public ModelAndView authorize(
 		@RequestParam(value="code", required = false) String authorizationCode,
 		@RequestParam(value="state", required = false) String state
 	) throws IOException {
+
+		// Get an instance of the TrustServiceManager class, responsible for communicating with PSCs
+		// and handling the OAuth flow.
 		TrustServicesManager manager = new TrustServicesManager();
 
+		// Complete the authentication process, recovering the session info to be used on the
+		// signature and the custom state (fileId).
 		TrustServiceSessionResult result = manager.completeAuth(authorizationCode, state);
 
 		// Recover file to be signed on custom state parameter.
@@ -79,7 +135,7 @@ public class PadesCloudOauthExpressController {
 		// Set PDF to be signed.
 		signer.setPdfToSign(StorageMock.getDataPath(fileToSign));
 
-		// Set trust session acquired on the following steps of this sample.
+		// Set trust session acquired on the previous steps of this sample.
 		signer.setTrustServiceSession(result.getSession());
 
 		// Set visual representation. We provide a Java class that represents the visual
