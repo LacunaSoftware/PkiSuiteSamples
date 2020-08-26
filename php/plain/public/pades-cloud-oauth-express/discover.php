caa6eb5f8cce6e589ae2ebd835c71861838d2080
EmilyS <emily.souza.rs@gmail.com>
2020-08-26 15:58:29 -0300

    [PHP/Plain] Added PAdES Cloud Signature

---
+++ php/plain/public/pades-cloud-oauth-express/discover.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * This action will be called after the user press the button "Search" on index page. It will
+ * search for all PSCs that have a certificate with the provided CPF. Thus, it will start the
+ * authentication process and return a URL to redirect the user to perform the authentication.
+ *
+ * After this action the user will be redirected, and to store the local data (fileId) to be user
+ * after the user returns to your application. We use the parameter "customState", the last
+ * parameter of the method discoverByCpfAndStartAuth(). This parameter will be recovered in the
+ * next action.
+ */
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Lacuna\PkiExpress\TrustServicesManager;
+use Lacuna\PkiExpress\TrustServiceSessionTypes;
+
+// Redirect URL where it's accessed after OAuth flow is finished.
+$REDIRECT_URL = "http://localhost:8000/pades-cloud-oauth-express/complete.php";
+
+// Only accepts POST requests.
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found", true, 404);
+    die();
+}
+
+// Get URL parameter "fileId"
+$fileId = !empty($_GET['fileId']) ? $_GET['fileId'] : null;
+
+// Retrieve input values from submitted form.
+$cpf = $_POST['cpf'];
+
+// Process cpf, removing all formatting.
+$plainCpf = str_replace([".","-"], "", $cpf);
+
+// Get an instance of the TrustServiceManager class, responsible for communicating with PSCs
+// and handling the OAuth flow.
+$manager = new TrustServicesManager();
+
+// Discover available PSCs.
+$services = $manager->discoverByCpfAndStartAuth($plainCpf, $REDIRECT_URL, TrustServiceSessionTypes::SIGNATURE_SESSION, $fileId);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include '../shared/head.php' ?>
+</head>
+<body>
+
+<?php include '../shared/menu.php' ?>
+
+<div class="container content">
+    <h2 class="ls-title">PAdES Signature using cloud certificate with PKI Express (OAuth Flow)</h2>
+
+    <?php if (sizeof($services) > 0) { ?>
+        <h5 class="ls-subtitle">Trusted services were found with this CPF</h5>
+        <div class="ls-content">
+            <form id="oauthFlowForm" action="/pades-cloud-oauth-express/complete/<?= $fileId ?>" method="POST">
+                <div class="form-group">
+                    <div class="px-0">
+                        <label for="serviceField">Choose a trust provider:</label>
+                        <select id="serviceField" name="service" class="form-control col col-sm-2" >
+                            <?php foreach($services as $s) { ?>
+                                <option value="<?= $s->authUrl ?>"><?= $s->serviceInfo->service->name ?> (<?= $s->serviceInfo->provider ?>)</option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-primary" onclick="redirectUser()">
+                    <i class="fas fa-file-signature"></i> Sign
+                </button>
+            </form>
+        </div>
+    <?php } else { ?>
+        <h5 class="ls-subtitle">No trusted services were found with this CPF</h5>
+        <div class="ls-content">
+            <p>Possible reasons include:
+                <ul>
+                    <li>The given CPF does not exist on trusted services</li>
+                    <li>The PKI Express was not correctly configured</li>
+                </ul>
+            </p>
+            <p>To configure the PKI Express you need to configure all providers by running the 
+                following commands on the terminal: <br/>
+                <code>pkie config --set trustServices:{provider}:clientId={value}</code><br/>
+                <code>pkie config --set trustServices:{provider}:clientSecret={value}</code>
+            </p>
+            <p>For customized providers you neet to also configure the endpoint:<br/>
+                <code>pkie config --set trustServices:{provider}:endpoint={value}</code>
+            </p>
+            <p>The standard providers are:
+                <ul>
+                    <li>BirdID (Soluti)</li>
+                    <li>ViDaaS (VALID)</li>
+                    <li>NeoID (SERPRO)</li>
+                    <li>RemoteID (Certisign)</li>
+                    <li>SafeID (Safeweb)</li>
+                </ul>
+            </p>
+            <button onclick="window.history.back()" class="btn btn-primary">Try Again</button>
+        </div>
+    <?php } ?>
+
+
+</div>
+
+<?php include '../shared/scripts.php' ?>
+
+<script fragment="scripts" type="text/javascript" inline="javascript">
+    function redirectUser() {
+        // Get redirect URL of the chosen provide.
+        var redirectUrl = $('#serviceField').val();
+        // Redirect user.
+        window.location = redirectUrl;
+    }
+</script>
+</body>
+</html>
---
+++ php/plain/public/pades-cloud-pwd-express/complete.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * This action is called after the form after the user press the button "Sign". This action will
+ * receive the user's CPF and current password.
+ */
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Lacuna\PkiExpress\StandardSignaturePolicies;
+use Lacuna\PkiExpress\PadesSigner;
+use Lacuna\PkiExpress\TrustServicesManager;
+use Lacuna\PkiExpress\TrustServiceSessionTypes;
+
+// Only accepts POST requests.
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found", true, 404);
+    die();
+}
+
+// Get URL parameter "fileId"
+$fileId = $_GET['fileId'];
+
+// Retrieve input values from submitted form.
+$cpf = $_POST['cpf'];
+$service = $_POST['service'];
+$password = $_POST['password'];
+
+// Process cpf, removing all formatting.
+$plainCpf = str_replace([".","-"], "", $cpf);
+
+// Get an instance of the TrustServiceManager class, responsible for communicating with PSCs
+// and handling the password flow.
+$manager = new TrustServicesManager();
+
+// Complete authentication using CPF and current password. The following method has three
+// sessionTypes:
+// - SINGLE_SIGNATURE: The returned token can only be used for one single signature request.
+// - MULTI_SIGNATURE: The returned token can only be used for one multi signature request.
+// - SIGNATURE_SESSION: The return token can only be used for one or more signature requests.
+$result = $manager->passwordAuthorize($service, $plainCpf, $password, TrustServiceSessionTypes::SIGNATURE_SESSION);
+
+// Get an instance of the PadesSigner class, responsible for receiving the
+// signature elements and performing the local signature.
+$signer = new PadesSigner();
+
+// Set PKI default options. (see Util.php)
+Util::setPkiDefaults($signer);
+
+// Set signature policy.
+$signer->signaturePolicy = StandardSignaturePolicies::PADES_BASIC_WITH_LTV;
+
+// Set PDF to be signed.
+$signer->setPdfToSign(StorageMock::getDataPath($fileId));
+
+// Set trust session acquired on the following steps of this sample.
+$signer->setTrustServiceSession($result->session);
+
+// The PKCS #12 certificate path.
+$signer->setPkcs12(StorageMock::getSampleCertificatePath());
+// Set the certificate's PIN.
+$signer->setCertPassword("1234");
+
+// Set a file reference for the stamp file. Note that this file can be
+// referenced later by "fref://stamp" at the "url" field on the visual
+// representation (see content/vr.json file or getVisualRepresentation($case)
+// method).
+$signer->addFileReference('stamp', StorageMock::getPdfStampPath());
+
+// Set visual representation. We provide a PHP class that represents the visual
+// representation model.
+$signer->setVisualRepresentation(PadesVisualElementsExpress::getVisualRepresentation());
+
+// Generate path for output file and add to signer object.
+$outputFile = StorageMock::generateFileId('pdf');
+$signer->setOutputFile(StorageMock::getDataPath($outputFile));
+
+// Perform the signature.
+$signer->sign();
+
+?><!DOCTYPE html>
+<html>
+<head>
+    <?php include '../shared/head.php' ?>
+</head>
+<body>
+
+<?php include '../shared/menu.php' ?>
+
+
+<div class="container content">
+    <div id="messagePanel"></div>
+
+    <h2 class="ls-title">PAdES Signature using cloud certificate with PKI Express (Password Flow)</h2>
+    <h5 class="ls-subtitle">File signed successfully! <i class="fas fa-check-circle text-success"></i></h5>
+
+    <div class="ls-content">
+        <h3>Actions:</h3>
+        <ul>
+            <li><a href="/download?fileId=<?= $outputFile ?>">Download the signed file</a></li>
+            <li><a href="/open-pades-express?fileId=<?= $outputFile ?>">Open/validate the signed file</a></li>
+            <li><a href="/pades-signature-express?fileId=<?= $outputFile ?>">Co-sign with another certificate</a></li>
+        </ul>
+    </div>
+</div>
+
+<? include '../shared/scripts.php' ?>
+
+</body>
+</html>
