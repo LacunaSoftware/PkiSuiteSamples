242622fb09a59eef43fd1764af1b78769cd65fe9
Danilo Bispo <danilobispogalvao@gmail.com>
2022-04-18 17:15:14 -0300

    Protótipo de POSTs do prepare e complete authentication; Criação de páginas protótipo

--- java/springmvc/src/main/java/com/lacunasoftware/pkisuite/controller/AuthenticationRestPkiCoreController.java
+++ java/springmvc/src/main/java/com/lacunasoftware/pkisuite/controller/AuthenticationRestPkiCoreController.java
@@ -6,13 +6,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.lacunasoftware.pkisuite.util.Util;
 import com.lacunasoftware.restpkicore.CompleteAuthenticationRequest;
+import com.lacunasoftware.restpkicore.DigestAlgorithmAndValueModel;
 import com.lacunasoftware.restpkicore.PrepareAuthenticationRequest;
+import com.lacunasoftware.restpkicore.PrepareAuthenticationResponse;
 import com.lacunasoftware.restpkicore.RestPkiService;
 import com.lacunasoftware.restpkicore.RestPkiServiceFactory;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -22,18 +25,33 @@ public class AuthenticationRestPkiCoreController {
 
     @GetMapping
     public String get(Model model, HttpServletResponse response){
-
-        // RestPkiService service = RestPkiServiceFactory.getService(Util.getRestPkiCoreOptions());
-
-        // PrepareAuthenticationRequest prepareAuthRequest = new PrepareAuthenticationRequest();
-
-        // Tomar cuidado com essa linha
-        // prepareAuthRequest.setSecurityContextId(UUID.fromString(Util.getSecurityContextId().getId()));
-    
-        // prepareAuthRequest.setIgnoreRevocationStatusUnknown(true);
-    
         // CompleteAuthenticationRequest completeAuthenticationRequest = new CompleteAuthenticationRequest();
         return "authentication-restpki-core/index";
+    }
+
+    @PostMapping
+    public String postPrepareAndCompleteAuthentication(Model model, HttpServletResponse response) throws Exception {
+        RestPkiService service = RestPkiServiceFactory.getService(Util.getRestPkiCoreOptions());
+
+        PrepareAuthenticationRequest prepareAuthRequest = new PrepareAuthenticationRequest();
+        // Tomar cuidado com essa linha
+        prepareAuthRequest.setSecurityContextId(UUID.fromString(Util.getSecurityContextId().getId()));
+        prepareAuthRequest.setIgnoreRevocationStatusUnknown(true);
+
+        // Executes POST request for prepareAuthentication (api/v2/authentication)
+        PrepareAuthenticationResponse prepareAuthResponse = service.prepareAuthentication(prepareAuthRequest);
+        System.out.println("Deu bom");
+        System.out.println(prepareAuthResponse.toString());
+
+        String state = prepareAuthResponse.getState();
+        // DigestAlgorithmAndValueModel toSignHash = prepareAuthResponse.getToSignHash();
+
+        CompleteAuthenticationRequest completeAuthRequest = new CompleteAuthenticationRequest();
+        
+        completeAuthRequest.setState(state);
+
+
+        return "authentication-restpki-core/success";
     }
 
 }
