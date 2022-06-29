888eb8e7d909975a539de5d28396c33d425f199e
Danilo Bispo <danilobispogalvao@gmail.com>
2022-06-29 12:07:04 -0300

    POST example done

--- nodejs/expressmvc/routes/signature-sessions-restpkicore.js
+++ nodejs/expressmvc/routes/signature-sessions-restpkicore.js
@@ -1,11 +1,11 @@
-const client = require("restpkicore-client-typescript");
+const { SignatureSessionsApi, Configuration } = require("restpkicore-client-typescript");
 const express = require('express');
 const axios = require("axios");
 const { Util } = require('../util');
 const { restPKIcore } = require("../config/default");
 const router = express.Router();
 
-var config = new client.Configuration();
+var config = new Configuration();
 config.apiKey = axios.create({
 	headers: {
 		'x-api-key': restPKIcore.apiKey
@@ -13,14 +13,25 @@ config.apiKey = axios.create({
 });
 config.basePath = restPKIcore.endpoint;
 
-signApi = new client.SignatureSessionsApi(config, config.basePath, config.apiKey);
-
-// signApi.apiSignatureSessionsIdGet("3fa85f64-5717-4562-b3fc-2c963f66afa6").then((response) => {
-//     console.log(response);
-// })
+signApi = new SignatureSessionsApi(config, config.basePath, config.apiKey);
 
 router.get('/', (req, res, next) => { 
-    res.render('signature-sessions-restpkicore');
+    var returnUrl = null;
+
+    // GET
+    // signApi.apiSignatureSessionsIdGet("3fa85f64-5717-4562-b3fc-2c963f66afa6").then((response) => {
+    //     console.log(response);
+    // }).catch((err) => next(err));
+
+    // POST
+    signApi.apiSignatureSessionsPost().then((response) => {
+        // console.log(response.data.redirectUrl);
+        returnUrl = response.data.redirectUrl
+        res.render('signature-sessions-restpkicore', {
+            returnUrl
+        });
+    }).catch((err) => next(err))
 });
+
 
 module.exports = router;
