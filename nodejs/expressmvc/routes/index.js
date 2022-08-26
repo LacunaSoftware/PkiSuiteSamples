b190df0ac97bb1f3f2c1804898b1f39ffdf865e2
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2022-08-26 16:28:35 -0300

    refactor(node/express): improve example with cloud providers to be simpler

--- nodejs/expressmvc/routes/index.js
+++ nodejs/expressmvc/routes/index.js
@@ -35,7 +35,6 @@ router.use('/pades-signature-restpki', require('./pades-signature-restpki'));
 router.use('/pades-wo-communication-restpki', require('./pades-wo-communication-restpki'));
 router.use('/pades-cloud-oauth-express', require('./pades-cloud-oauth-express'));
 router.use('/pades-cloud-pwd-express', require('./pades-cloud-pwd-express'));
-router.use('/pades-cloud-pki-paraguay-express', require('./pades-cloud-pki-paraguay-express'));
 router.use('/printer-version-cades-express', require('./printer-version-cades-express'));
 router.use('/printer-version-pades-restpki', require('./printer-version-pades-restpki'));
 router.use('/printer-version-pades-express', require('./printer-version-pades-express'));
@@ -51,6 +50,6 @@ router.use('/xml-nfe-signature-express', require('./xml-nfe-signature-express'))
 router.use('/xml-server-key-express', require('./xml-server-key-express'));
 router.use('/xml-signature-restpki', require('./xml-signature-restpki'));
 router.use('/xml-signature-express', require('./xml-signature-express'));
-router.use('/xml-cloud-pki-paraguay-express', require('./xml-cloud-pki-paraguay-express'));
+router.use("/tsp-callback", require('./tsp-callback'));
 
 module.exports = router;
--- nodejs/expressmvc/routes/tsp-callback.js
+++ nodejs/expressmvc/routes/tsp-callback.js
@@ -1,17 +1,32 @@
-const express = require('express');
+const express = require("express");
+const { TrustServicesManager } = require("pki-express");
+
+const { Util } = require("../util");
 
 const router = express.Router();
 
 /**
  * GET /
  *
- * The route to the application's homepage.
+ * The route for .
  */
-router.get('/', (req, res) => {
-    // Get parameters from urls
-    const { code, state } = req.query;
+router.get("/", async (req, res) => {
+	const { code, state } = req.query;
 
-    res.render('home');
+	// Get an instance of the TrustServiceManager class,
+	// responsible for communicating with PSCs and handling
+	// the OAuth flow.
+	const manager = new TrustServicesManager();
+
+	// Set PKI default options (see util.js).
+	Util.setPkiDefaults(manager);
+
+	// Retrieve session from customState
+	const result = await manager.getCustomState(state);
+	const sessionId = result.customState;
+
+	// Render page.
+	res.render("tsp-callback", { code, state, sessionId });
 });
 
 module.exports = router;
