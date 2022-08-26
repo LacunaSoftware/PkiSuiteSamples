569b26e3f7036ba8e1e03ad956e93495b1ec5e07
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2022-08-26 19:45:09 -0300

    feat(node/express): add xml sample with cloud certificate

--- nodejs/expressmvc/routes/tsp-callback.js
+++ nodejs/expressmvc/routes/tsp-callback.js
@@ -8,17 +8,18 @@ const router = express.Router();
 /**
  * GET /
  *
- * The route for .
+ * The route used for callback for cloud samples.
  */
 router.get("/", async (req, res) => {
 	const { code, state } = req.query;
 
-	// Get an instance of the TrustServiceManager class,
-	// responsible for communicating with PSCs and handling
-	// the OAuth flow.
+	// Get an instance of the TrustServiceManager class, responsible for
+	// communicating with PSCs and handling the OAuth flow. And set common
+	// configuration with setPkiDefaults (see util.js)
 	const manager = new TrustServicesManager();
+	Util.setPkiDefaults(manager);
 
-	// Set PKI default options (see util.js).
+	// Set PKI default options (see util.js)
 	Util.setPkiDefaults(manager);
 
 	// Retrieve session from customState
--- nodejs/expressmvc/routes/xml-cloud-oauth-express.js
+++ nodejs/expressmvc/routes/xml-cloud-oauth-express.js
@@ -5,16 +5,13 @@ const uuidv4 = require("uuid/v4");
 
 const {
 	StandardSignaturePolicies,
-	PadesSigner,
+	XmlSigner,
 	TrustServicesManager,
 	TrustServiceSessionTypes,
 } = require("pki-express");
 
 const { Util } = require("../util");
-const { StorageMock } = require("../storage-mock");
-const {
-	PadesVisualElementsExpress,
-} = require("../pades-visual-elements-express");
+const { StorageMock, SampleDocs } = require("../storage-mock");
 
 /**
  * This sample is responsible to perform a OAuth flow to communicate with PSCs to perform a
@@ -49,21 +46,13 @@ const APP_ROOT = process.cwd();
 const REDIRECT_URL = "http://localhost:3000/tsp-callback";
 
 /**
- * GET /pades-cloud-oauth-express
+ * GET /xml-cloud-oauth-express
  *
  * This action will render a page for the user to choose one of the available
  * providers, which were configured by the command above.
  *
  */
 router.get("/", async (req, res, next) => {
-	const { fileId } = req.query;
-	if (!StorageMock.existsSync({ fileId: fileId })) {
-		const notFound = new Error("The fileId was not found");
-		notFound.status = 404;
-		next(notFound);
-		return;
-	}
-
 	// Get an instance of the TrustServiceManager class, responsible for
 	// communicating with PSCs and handling the OAuth flow. And set common
 	// configuration with setPkiDefaults (see util.js).
@@ -88,15 +77,15 @@ router.get("/", async (req, res, next) => {
 	const authParameters = result.authParameters;
 
 	// Render the result page.
-	res.render("pades-cloud-oauth-express", {
-		fileId,
+	res.render("xml-cloud-oauth-express", {
+		sampleId: SampleDocs.SAMPLE_XML,
 		authParameters,
 		sessionId,
 	});
 });
 
 /**
- * POST /pades-cloud-oauth-express
+ * POST /xml-cloud-oauth-express
  *
  * This action will complete the authentication process and
  * create a signature using a session token returned by user.
@@ -108,67 +97,47 @@ router.post("/", async (req, res, next) => {
 	// Get parameters from urls
 	const code = req.body.codeField;
 	const state = req.body.stateField;
-	const fileId = req.body.fileIdField;
-
-	// Verify if the provided fileId exists.
-	if (!StorageMock.existsSync({ fileId: fileId })) {
-		const notFound = new Error("The fileId was not found");
-		notFound.status = 404;
-		next(notFound);
-		return;
-	}
 
 	try {
-		// Get an instance of the TrustServiceManager class,
-		// responsible for communicating with PSCs and handling
-		// the OAuth flow.
+		// Get an instance of the TrustServiceManager class, responsible for
+		// communicating with PSCs and handling the OAuth flow. And setting
+		// common configuration with setPkiDefaults (see util.js)
 		const manager = new TrustServicesManager();
+		Util.setPkiDefaults(manager);
 
 		// Complete the authentication process, recovering the
 		// session info to be used on the signature and the
 		// custom state (fileId).
 		const result = await manager.completeAuth(code, state);
 
-		// Get an instance of the PadesSigner class, responsible for receiving
+		// Get an instance of the XmlSigner class, responsible for receiving
 		// the signature elements and performing the local signature. And
-		// setting common configuration with setPkiDefaults (see util.js).
-		const signer = new PadesSigner();
+		// setting common configuration with setPkiDefaults (see util.js)
+		const signer = new XmlSigner();
 		Util.setPkiDefaults(signer);
 
-		// Set signature policy.
-		signer.signaturePolicy = StandardSignaturePolicies.PADES_BASIC_WITH_LTV;
+		// Set signature policy
+		signer.signaturePolicy = StandardSignaturePolicies.XADES_BES;
 
-		// Set PDF to be signed.
-		await signer.setPdfToSignFromPath(StorageMock.getDataPath(fileId));
+		// Set the XML to be signed.
+		await signer.setXmlToSignFromPath(
+			StorageMock.getSampleDocPath(SampleDocs.SAMPLE_XML)
+		);
 
 		// Set trust session acquired on the following steps of this sample.
 		signer.trustServiceSession = result.session;
 
-		// Set a file reference for the stamp file. Note that file can be
-		// referenced later by "fref://{alias}" at the "url" field on the visual
-		// representation (see public/vr.json or getVisualRepresentation()
-		// method).
-		await signer.addFileReference("stamp", StorageMock.getPdfStampPath());
-
-		// Set visual reference. We provide a dictionary that represents the
-		// visual representation JSON model.
-		await signer.setVisualRepresentation(
-			PadesVisualElementsExpress.getVisualRepresentation()
-		);
-
 		// Generate path for output file and add the signature finisher.
-		const outputFile = `${uuidv4()}.pdf`;
+		const outputFile = `${uuidv4()}.xml`;
 		StorageMock.createAppDataSync(); // Make sure the "app-data" folder exists.
 		signer.outputFile = path.join(APP_ROOT, "app-data", outputFile);
 
 		// Perform the signature.
 		const signerCert = await signer.sign(true);
 
-		Util.setExpiredPage(res);
-
 		// Render the result page.
-		res.render("pades-cloud-oauth-express/complete", {
-			signedPdf: outputFile,
+		res.render("xml-cloud-oauth-express/complete", {
+			signedXml: outputFile,
 			signerCert,
 		});
 	} catch (err) {
