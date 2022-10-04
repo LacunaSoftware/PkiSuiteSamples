fec7486909a91d765482e3c401b9b7fb442ef6a3
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2022-10-04 20:19:00 -0300

    [dotnet/mvc] remove try catch to handle exception globally

--- dotnet/mvc/PkiSuiteAspNetMvcSample/Controllers/PadesServerKeyRestCoreController.cs
+++ dotnet/mvc/PkiSuiteAspNetMvcSample/Controllers/PadesServerKeyRestCoreController.cs
@@ -15,41 +15,34 @@ namespace PkiSuiteAspNetMvcSample.Controllers {
 	public class PadesServerKeyRestCoreController : BaseController {
 
 		public async Task<ActionResult> Index(string userfile) {
+			var restPkiService = Util.GetRestPkiService();
+
+			// Read certificate and upload content to REST PKI Core
+			var certificatePath = StorageMock.GetServerCertificatePath();
+			var certificate = new X509Certificate2(certificatePath, "1234");
+			var certificateReference = CertificateReference.FromX509Certificate2(certificate);
+
+			// Get reference for the file to be signed
+			if (!StorageMock.TryGetFile(userfile, out string userfilePath)) {
+				return HttpNotFound();
+			}
+			var fileReference = FileReference.FromFile(userfilePath, userfile);
+
+			// Prepare signature in REST PKI Core service and get signature parameters
+			var signatureOptions = new PrepareSignatureOptions() { SecurityContextId = Util.GetSecurityContextId() };
+			var prepareSignatureResult = await restPkiService.PrepareSignatureAsync(fileReference, certificateReference, signatureOptions);
+
+			// Perform RSA signature using signature parameters and certificate reference
+			var toSignHash = prepareSignatureResult.ToSignHash.Value;
+			var signature = certificate.GetRSAPrivateKey().SignHash(toSignHash, prepareSignatureResult.ToSignHash.Algorithm.HashAlgorithmName, RSASignaturePadding.Pkcs1);
+
+			// Complete signature in REST PKI Core service
+			var signedDocument = await restPkiService.CompleteSignatureAsync(prepareSignatureResult.State, signature);
+
+			// Store file
 			string signedFilename;
-
-			try {
-				var restPkiService = Util.GetRestPkiService();
-
-				// Read certificate and upload content to REST PKI Core
-				var certificatePath = StorageMock.GetServerCertificatePath();
-				var certificate = new X509Certificate2(certificatePath, "1234");
-				var certificateReference = CertificateReference.FromX509Certificate2(certificate);
-
-				// Get reference for the file to be signed
-				if (!StorageMock.TryGetFile(userfile, out string userfilePath)) {
-					return HttpNotFound();
-				}
-				var fileReference = FileReference.FromFile(userfilePath, userfile);
-
-				// Prepare signature in REST PKI Core service and get signature parameters
-				var signatureOptions = new PrepareSignatureOptions() { SecurityContextId = Util.GetSecurityContextId() };
-				var prepareSignatureResult = await restPkiService.PrepareSignatureAsync(fileReference, certificateReference, signatureOptions);
-
-				// Perform RSA signature using signature parameters and certificate reference
-				var toSignHash = prepareSignatureResult.ToSignHash.Value;
-				var signature = certificate.GetRSAPrivateKey().SignHash(toSignHash,prepareSignatureResult.ToSignHash.Algorithm.HashAlgorithmName, RSASignaturePadding.Pkcs1);
-
-				// Complete signature in REST PKI Core service
-				var signedDocument = await restPkiService.CompleteSignatureAsync(prepareSignatureResult.State, signature);
-
-				// Store file
-				using (var docStream = await signedDocument.SignedFile.OpenReadAsync()) {
-					signedFilename = StorageMock.Store(docStream, ".pdf");
-				}
-					
-			} catch (Exception ex) {
-				ModelState.AddModelError("", ex.ToString());
-				return View();
+			using (var docStream = await signedDocument.SignedFile.OpenReadAsync()) {
+				signedFilename = StorageMock.Store(docStream, ".pdf");
 			}
 
 			return View(new SignatureInfoModel() {
