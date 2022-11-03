5018d2eda42717c2dcac412f5cb0c0ea5d669fc9
Danilo Bispo <danilobispogalvao@gmail.com>
2022-11-03 15:12:47 -0300

    Licenses update: November (part 2)

--- nodejs/expressmvc/config/default.js
+++ nodejs/expressmvc/config/default.js
@@ -51,7 +51,7 @@ module.exports = {
 		//     >>>> PASTE YOUR AMPLIA API KEY BELOW <<<<
 		// ========================================================
 		apiKey: 'pki-suite-samples-01|ee9a3416cc231a4ab177ccdee926d03f658a83d3673398514e99e21f590e8744',
-		// This is a TRIAL API key to use Amplia. It will expire at 31/12/2022.
+	// This is a TRIAL API key to use Amplia. It will expire at 31/12/2022.
 		// If the Amplia's samples do not work please contact our support by email:
 		// suporte@lacunasoftware.com
 		//
