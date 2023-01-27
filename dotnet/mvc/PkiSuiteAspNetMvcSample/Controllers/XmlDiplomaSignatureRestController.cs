e3f5fc4cf268cb76f6c16dfea6cdcf85a4016254
Daniel Rufino <daniel.s.rufino@gmail.com>
2023-01-27 14:43:08 -0300

    DiplomaSignature Working

--- dotnet/mvc/PkiSuiteAspNetMvcSample/Controllers/XmlDiplomaSignatureRestController.cs
+++ dotnet/mvc/PkiSuiteAspNetMvcSample/Controllers/XmlDiplomaSignatureRestController.cs
@@ -4,6 +4,7 @@ using PkiSuiteAspNetMvcSample.Classes;
 using PkiSuiteAspNetMvcSample.Models.Rest;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -13,6 +14,7 @@ namespace PkiSuiteAspNetMvcSample.Controllers
 {
     public class XmlDiplomaSignatureRestController : Controller
     {
+
         public async Task<ActionResult> Index()
         {
 
@@ -36,7 +38,101 @@ namespace PkiSuiteAspNetMvcSample.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> Index(SignatureModel model)
+        public async Task<ActionResult> DadosDiploma(SignatureModel model)
+        {
+
+            var signatureFinisher = new XmlSignatureFinisher(Util.GetRestPkiClient())
+            {
+                Token = model.Token
+            };
+            var signedDiplomaXml = await signatureFinisher.FinishAsync();
+
+            var fileId = StorageMock.Store(signedDiplomaXml, ".xml");
+
+            var signerCert = signatureFinisher.GetCertificateInfo();
+
+
+            return View( new SignatureXmlModel()
+            {
+                UserFile = fileId,
+            });
+
+        }
+
+        public async Task<ActionResult> Registro(string file)
+        {
+            string userfilePath;
+            if (!StorageMock.TryGetFile(file, out userfilePath))
+            {
+                return HttpNotFound();
+            }
+
+
+            var signatureStarter = new XmlElementSignatureStarter(Util.GetRestPkiClient());
+
+            signatureStarter.SetXml(userfilePath);
+
+            signatureStarter.SetToSignElementId("Dip35141214314050000662550010001084271182362300");
+
+            signatureStarter.SetSignaturePolicy(new Guid("826b3a00-3400-4236-90d9-40917b0cc83b"));
+
+            signatureStarter.SetSecurityContext(Util.GetSecurityContextId());
+
+            var token = signatureStarter.StartWithWebPki();
+
+            return View(new SignatureModel()
+            {
+                Token = token,
+            });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> DadosRegistro(SignatureModel model)
+        {
+
+
+            var signatureFinisher = new XmlSignatureFinisher(Util.GetRestPkiClient())
+            {
+                Token = model.Token
+            };
+            var signedDiplomaXml = await signatureFinisher.FinishAsync();
+
+
+            var fileId = StorageMock.Store(signedDiplomaXml, ".xml");
+
+            return View("DadosRegistro", new SignatureXmlModel()
+            {
+                UserFile = fileId,
+            });
+
+        }
+        public async Task<ActionResult> FullDiploma(string file)
+        {
+            string userfilePath;
+            if (!StorageMock.TryGetFile(file, out userfilePath))
+            {
+                return HttpNotFound();
+            }
+
+            var signatureStarter = new FullXmlSignatureStarter(Util.GetRestPkiClient());
+
+            signatureStarter.SetXml(userfilePath);
+
+
+            signatureStarter.SetSignaturePolicy(new Guid("5b8c0710-006d-434d-bf35-12292b56b90a"));
+
+            signatureStarter.SetSecurityContext(Util.GetSecurityContextId());
+
+            var token = signatureStarter.StartWithWebPki();
+
+            return View(new SignatureModel()
+            {
+                Token = token,
+            });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Diploma(SignatureModel model)
         {
 
             var signatureFinisher = new XmlSignatureFinisher(Util.GetRestPkiClient())
@@ -49,19 +145,12 @@ namespace PkiSuiteAspNetMvcSample.Controllers
 
             var fileId = StorageMock.Store(signedXml, ".xml");
 
-            return View("SignatureInfo", new SignatureInfoModel()
+            return View(new SignatureXmlModel()
             {
-                File = fileId,
-                SignerCertificate = signerCert
+                UserFile = fileId
             });
 
-
-
-
-
-
-
-
         }
+
     }
 }
---
+++ dotnet/mvc/PkiSuiteAspNetMvcSample/Models/Rest/SignatureXmlModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PkiSuiteAspNetMvcSample.Models.Rest
+{
+    public class SignatureXmlModel
+    {
+        public string UserFile { get; set; }
+    }
+}
