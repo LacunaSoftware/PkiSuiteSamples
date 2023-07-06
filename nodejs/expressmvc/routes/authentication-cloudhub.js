58d84b48a0c2bd5fe5256f6eaa5198513c6c8afc
Danilo Bispo <danilobispogalvao@gmail.com>
2023-07-06 17:10:44 -0300

    Authentication example (outputs an error)

--- nodejs/expressmvc/routes/authentication-cloudhub.js
+++ nodejs/expressmvc/routes/authentication-cloudhub.js
@@ -1,12 +1,12 @@
 const { CloudHubClient, TrustServiceSessionTypes } = require("cloudhub-client");
 const express = require("express");
 const { Util } = require("../util");
-const {
-	Authentication
-} = require("restpki-client");
+const { Authentication } = require("restpki-client");
 
 const uuidv4 = require("uuid/v4");
 const path = require("path");
+const { result } = require("lodash");
+const { time } = require("console");
 
 const app = express();
 
@@ -14,6 +14,7 @@ const router = express.Router();
 const APP_ROOT = process.cwd();
 
 this.client = Util.getCloudhubClient();
+this.restPkiClient = Util.getRestPkiClient();
 
 /*
  * GET /authentication-cloudhub
@@ -23,35 +24,10 @@ this.client = Util.getCloudhubClient();
  */
 router.get("/", async (req, res, next) => {
 	// Get an instance of the Authentication class.
-	const auth = new Authentication(Util.getRestPkiClient());
+	const auth = new Authentication(this.restPkiClient);
 
-	// Call the startWithWebPki() method, which initiates the authentication.
-	// This yields the "token", a 22-character case-sensitive URL-safe string,
-	// which represents this authentication process. We'll use this value to call
-	// the signWithRestPki() method on the Web PKI component
-	// (see public/javascripts/signature-form.js) and also call the
-	// completeWithWebPki() method on "complete" step. This should not be
-	// mistaken with the API access token. We have encapsulated the security
-	// context choice on util.js.
-	auth.startWithWebPki(Util.getSecurityContextId(res.locals.environment))
-		.then((token) => {
-			// The token acquired can only be used for a single authentication.
-			// In order to retry authenticating it is necessary to get a new token.
-			// This can be a problem if the user uses the back button of the
-			// browser, since the browser might show a cached page that we rendered
-			// previously, with a now stale token. To prevent this from happening,
-			// we call the function setExpiredPage(), located in util.js, which sets
-			// HTTP headers to prevent caching of the page.
-			Util.setExpiredPage(res);
-
-			// Render the authentication page.
-
-			// Render the authentication page.
-			res.render("authentication-cloudhub", {
-				token,
-			});
-		})
-		.catch((err) => next(err));
+	// Render the authentication page.
+	res.render("authentication-cloudhub", {});
 });
 
 /*
@@ -62,19 +38,18 @@ router.get("/", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
 	try {
 		const cpf = req.body.cpf;
-        const token = req.body.token
 
 		// remove all dots and hyphens
 		const new_cpf = cpf.replace(/[.-]/g, "");
 
 		const sessionRes = await this.client.createSessionAsync({
 			identifier: new_cpf,
-			redirectUri: `http://localhost:3000/authentication-cloudhub/session-result?token=${token}`,
+			redirectUri: `http://localhost:3000/authentication-cloudhub/session-result`,
 			type: TrustServiceSessionTypes.SingleSignature,
 		});
 
 		res.render("authentication-cloudhub/service-select", {
-			sessionRes
+			sessionRes,
 		});
 	} catch (err) {
 		next(err);
@@ -88,61 +63,81 @@ router.post("/", async (req, res, next) => {
  * process
  */
 router.get("/session-result", async (req, res, next) => {
+	const auth = new Authentication(this.restPkiClient);
 	const session = req.query.session;
-    const token = req.query.token;
-
 	const cert = await this.client.getCertificateAsync(session);
 
-	this.client.signHashAsync({
-		hash: token,
-		session: session
-	}).then(result => {
-		const auth = new Authentication(Util.getRestPkiClient());
-
-		
-		
-		// Call the completeWithWebPki() method with the token, which finalizes the
-		// authentication process. The call yields a ValidationResults which denotes
-		// whether the authentication was success or not.
-		auth.completeWithWebPki(token)
+	// Call the startWithWebPki() method, which initiates the authentication.
+	// This yields the "token", a 22-character case-sensitive URL-safe string,
+	// which represents this authentication process. We'll use this value to call
+	// the signWithRestPki() method on the Web PKI component
+	// (see public/javascripts/signature-form.js) and also call the
+	// completeWithWebPki() method on "complete" step. This should not be
+	// mistaken with the API access token. We have encapsulated the security
+	// context choice on util.js.
+	auth.start(Util.getSecurityContextId(res.locals.environment))
 		.then((result) => {
-			// Check the authentication result.
-			if (!result.validationResults.isValid()) {
-				// The toString() method of the ValidationResults object can be used to
-				// obtain the checks performed, but the string contains tabs and new
-				// line characters for formatting, which we'll convert to <br>'s and
-				// &nbsp;'s.
-				const vrHtml = result.validationResults
-				.toString()
-				.replace(/\n/g, "<br>")
-				.replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;");
-				
-				// If the authentication was not successful, we render a page showing
-				// what went wrong.
-				res.render("authentication-cloudhub/failed", {
-					vrHtml,
+			const hash = result.toSignHash;
+			const token = result.token;
+			this.client
+				.signHashAsync({
+					hash: hash,
+					session: session.toString(),
+					digestAlgorithmOid: result.digestAlgorithmOid,
+				})
+				.then((result) => {
+					const payload = {
+						signature: result.toString(),
+						certificate: cert.toString(),
+					};
+					auth.complete(token, JSON.stringify(payload))
+						.then((result) => {
+							
+							// TODO: analyse this object
+							// console.log(result.validationResults);
+
+							// Check the authentication result.
+							if (!result.validationResults.isValid()) {
+								// The toString() method of the ValidationResults object can be used to
+								// obtain the checks performed, but the string contains tabs and new
+								// line characters for formatting, which we'll convert to <br>'s and
+								// &nbsp;'s.
+								const vrHtml = result.validationResults
+									.toString()
+									.replace(/\n/g, "<br>")
+									.replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;");
+
+								// If the authentication was not successful, we render a page showing
+								// what went wrong.
+								res.render("authentication-cloudhub/failed", {
+									vrHtml,
+								});
+							} else {
+								// At this point, you have assurance tha the certificate is valid
+								// according to the TrustArbitrator you selected when starting the
+								// authentication and that the user is indeed the certificate's subject.
+								// Now, you'd typically query your database for a user that matches one of
+								// the certificate's fields, such as userCert.emailAddress or
+								// userCert.pkiBrazil.cpf (the actual field to be used as key depends on
+								// your application's business logic) and set the user ID on the cookie
+								// as if it were the user ID.
+								const userCert = cert;
+
+								// Redirect to the initial page with the user logged in.
+								res.render("authentication-cloudhub/success", {
+									userCert,
+								});
+							}
+						})
+						.catch((err) => next(err));
+				})
+				.catch((err) => {
+					next(err);
 				});
-			} else {
-				// At this point, you have assurance tha the certificate is valid
-				// according to the TrustArbitrator you selected when starting the
-				// authentication and that the user is indeed the certificate's subject.
-				// Now, you'd typically query your database for a user that matches one of
-				// the certificate's fields, such as userCert.emailAddress or
-				// userCert.pkiBrazil.cpf (the actual field to be used as key depends on
-				// your application's business logic) and set the user ID on the cookie
-				// as if it were the user ID.
-				const userCert = cert
-				
-				// Redirect to the initial page with the user logged in.
-				res.render("authentication-cloudhub/success", {
-					userCert,
-				});
-			}
 		})
-		.catch((err) => next(err));
-	});
-	
-	router.get("/complete", async (req, res, next) => {});
+		.catch((err) => {
+			next(err);
+		});
 });
-	
+
 module.exports = router;
