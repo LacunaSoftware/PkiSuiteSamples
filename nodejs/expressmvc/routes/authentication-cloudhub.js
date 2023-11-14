01e6a9fd1b9662e6f87304eb4dc3832a8b204cd3
Danilo Bispo <danilobispogalvao@gmail.com>
2023-11-14 12:04:00 -0300

    Small fix

--- nodejs/expressmvc/routes/authentication-cloudhub.js
+++ nodejs/expressmvc/routes/authentication-cloudhub.js
@@ -37,8 +37,6 @@ router.get("/", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
 	try {
 		const cpf = req.body.cpf;
-		console.log(restpki.VERSION);
-
 		// remove all dots and hyphens
 		const new_cpf = cpf.replace(/[.-]/g, "");
 
