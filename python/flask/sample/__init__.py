e20cf8b4ef1a19743c713638c060c2c8846ea1bd
Danilo Bispo <danilobispogalvao@gmail.com>
2024-08-05 10:54:41 +0000

    Added config for sample to work

--- python/flask/sample/__init__.py
+++ python/flask/sample/__init__.py
@@ -27,6 +27,7 @@ if sys.version_info[0] < 3:
 def create_app():
     app = Flask(__name__)
 
+    app.env = os.environ["FLASK_ENV"]
     # Select configuration based on the application environment.
     if app.env not in config:
         raise Exception('The "%s" environment is not supported' % app.env)
